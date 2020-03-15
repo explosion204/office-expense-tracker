d3ac5dfaa049bbaafd1743532ac1422f3301961c
explosion204 <dzmitriy20magic@gmail.com>
2020-03-15 16:31:14 +0300

    overview tab: interacting with list widgets

--- gui/mainwindow.cpp
+++ gui/mainwindow.cpp
@@ -28,26 +28,39 @@ void MainWindow::initViews()
     {
         ListWidgetItem *dep_item = new ListWidgetItem(department->getId(), department->getTitle());
         ui->departmentsListWidget->addItem(dep_item);
-        for (auto expense: department->getExpenses())
-        {
-            ListWidgetItem *exp_item = new ListWidgetItem(expense->getId(), expense->getName());
-            ui->expensesListWidget->addItem(exp_item);
-        }
     }
 }
 
 void MainWindow::on_departmentsListWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous)
 {
+    ui->departmentInfo->clear();
     int item_id = dynamic_cast<ListWidgetItem*>(current)->getItemId();
-    /*
-     * Title:
-     * Identification number:
-     * Members count:
-    */
-
+    Department *department = Aggregator::getInstance()->getDepartment(item_id);
+    QString title = department->getTitle();
+    int members_count = department->getMembersCount();
+    ui->departmentInfo->setText("Title: " + title + "\nID: " + QString(item_id) + "\nMembers count: " + QString(members_count));
+    ui->expensesListWidget->clear();
+    for (auto expense: department->getExpenses())
+    {
+        ListWidgetItem *exp_item = new ListWidgetItem(expense->getId(), expense->getName());
+        ui->expensesListWidget->addItem(exp_item);
+    }
 }
 
 void MainWindow::on_expensesListWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous)
 {
-
+    int item_id = dynamic_cast<ListWidgetItem*>(current)->getItemId();
+    int department_id = dynamic_cast<ListWidgetItem*>(ui->departmentsListWidget->currentItem())->getItemId();
+    Expense *expense = Aggregator::getInstance()->getDepartment(department_id)->getExpense(item_id);
+    /*
+     * Name:
+     * ID:
+     * Department ID:
+     * Description:
+     * Limit value per month:
+     * Current value:
+    */
+    ui->expenseInfo->setText("Name: " + expense->getName() + "\nID: " + QString(expense->getId()) + "\nDepartment ID: " + QString(expense->getDepartmentId()) +
+                             "\nDescription: " + expense->getDescription() + "\nLimit value per month: " + QString(expense->getLimit()) + "\nCurrent value: " +
+                             QString(expense->getValue()));
 }
